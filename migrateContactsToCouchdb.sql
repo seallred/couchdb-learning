82e6bb7efdccd5e4d92badab1af6b48152abdeee
seallred <seallred@gmail.com>
2016-03-18 12:36:47 -0500

    Playing with couchDb
    Trying to learn a little bit about couchDb

---
+++ migrateContactsToCouchdb.sql
@@ -0,0 +1,27 @@
+select * from cam.contact fetch first 10 rows only;
+
+select '{' CONCAT
+X'0A' CONCAT
+'   "srcContactId" : ' CONCAT
+RTRIM(contact_id) || ',' CONCAT
+X'0A' CONCAT
+'   "srcAppl" : "' CONCAT
+RTRIM(src_appl) || '",' CONCAT
+X'0A' CONCAT
+'   "firstName" : "' CONCAT
+RTRIM(first_name) || '",' CONCAT
+X'0A' CONCAT
+'   "lastName" : "' CONCAT
+RTRIM(last_name) || '",' CONCAT
+X'0A' CONCAT
+'   "emailAddress" : "' CONCAT
+RTRIM(email_address) || '",' CONCAT
+X'0A' CONCAT
+'   "ibmUid" : "' CONCAT
+RTRIM(coalesce(ibm_uid,'')) || '"' CONCAT
+X'0A' CONCAT
+'},' 
+from cam.contact fetch first 20 rows only;
+
+
+
